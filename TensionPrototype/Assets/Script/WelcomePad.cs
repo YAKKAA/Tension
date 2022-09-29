52b9e168887f167068a9fccfe716728a11ada049
Bangyu <bangyuwang@qq.com>
2022-09-29 15:43:24 -0400

    audio set up

--- TensionPrototype/Assets/Script/WelcomePad.cs
+++ TensionPrototype/Assets/Script/WelcomePad.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class WelcomePad : MonoBehaviour
 {   
     public GameObject enemy;
+    public AudioClip gameover;
+
+    AudioSource audiosource;
 
     private Enemy enemyScript;
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("oops");
         enemyScript.isChasing = true;
+        audiosource.PlayOneShot(gameover, 1.0f);
     }
-
     void Start() {
         enemyScript = enemy.GetComponent<Enemy>();
+        audiosource = GetComponent<AudioSource>();
     }
 }
